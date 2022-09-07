5096c6b943a425f2ac687eea46ad8a7815694571
FauziIrhamPulungan <fauzyirham24@gmail.com>
2022-09-07 21:12:06 +0700

    Done upload file

---
+++ CH3-Fauzi/index.js
@@ -0,0 +1,14 @@
+const Soal1 = require("./soal1");
+new Soal1(10, 12);
+
+// const Soal2 = require("./soal2-3");
+
+// const shark = new Soal2.Shark("Hiu", "10", "Berenang");
+// console.log(shark.introduce());
+
+// const cat = new Soal2.Cat("Kucing", "20", "Berjalan");
+// console.log(cat.introduce());
+
+// const dog = new Soal2.Dog("Anjing", "25", "Berjalan", "Andi");
+// console.log(dog.introduce());
+// console.log(dog.GreetMaster())
