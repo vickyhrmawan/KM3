6503c6dfd735abd2eccd9c56f95af4b2ef273375
luthfiahmdf <luthfiahmd@gmail.com>
2022-09-07 15:23:37 +0700

    Modified soal1 + add soal2

--- soal1.js
+++ soal1.js
@@ -1,12 +1,13 @@
-var emptyShip = new Ship(10, 15);
+var emptyShip = new Ship(15, 5);
 var emptyShip = new Ship(10, 5);
 function Ship(draft, crew) {
   this.draft = draft;
   this.crew = crew;
 
-  draft = draft + crew * 1.5;
+  draft = crew * 1.5 + draft;
 
   draft > 20
     ? console.log("Banyak Nih muatan kapalnya")
     : console.log("Lebih Banyak Muatan Orangnya");
+  console.log(draft);
 }
---
+++ soal2.js
@@ -0,0 +1,70 @@
+class Animal {
+  constructor(name, age, legs, species, status) {
+    this.name = "Jajang";
+    this.age = 12;
+    this.legs = legs;
+    this.species = species;
+    this.status = status;
+  }
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+  }
+}
+
+class Shark extends Animal {
+  constructor() {
+    super(...arguments);
+    this.name = "Hiu";
+    this.age = 10;
+    this.legs = 0;
+    this.species = "shark";
+    this.status = "Berenang";
+  }
+  introduce() {
+    return `Hello saya ${this.name} umur ${this.age},kaki ${this.legs},status ${this.status},spesies ${this.species}`;
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(...arguments);
+    this.name = "kucing";
+    this.age = 2;
+    this.legs = 4;
+    this.species = "cat";
+    this.status = "Berlari";
+  }
+
+  introduce() {
+    return (
+      `Halo saya ${this.name},Umur ku ${this.age} tahun,aku ${this.status} dengan ${this.legs} kaki` +
+      " .Meow meow!"
+    );
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(...arguments);
+    this.legs = 4;
+    this.species = "dog";
+    this.status = "berjalan";
+    this.master = "Jajang";
+    this.greetMaster();
+  }
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
+}
+
+var shark1 = new Shark();
+console.log(shark1.introduce());
+
+var dog1 = new Dog();
+console.log(dog1.greetMaster());
+
+var cat1 = new Cat();
+console.log(cat1.introduce());
+
+var animal1 = new Animal();
+console.log(animal1.introduce("jajang", 12));
