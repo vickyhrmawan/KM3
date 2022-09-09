c92baedab0a3d27fc55e455a6c1e67f822164e0e
Kris Adiwinata <99080684+FeaRisk@users.noreply.github.com>
2022-09-09 14:17:44 +0700

    create class Animal

---
+++ CH3-Kris/soal2.js
@@ -0,0 +1,12 @@
+class Animal {
+    constructor(name, age, status, legs, species ){
+        this.name = name
+        this.age = age
+        this.legs = legs
+        this.species = species
+        this.status = status
+    }
+    introduce() {
+        return `Hello, my name is ${this.name} and I am ${this.age} years old.`
+    }
+}
