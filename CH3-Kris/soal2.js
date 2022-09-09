c609afe549afd59c3dc66cfe0cf6e5b0fc40e525
Kris Adiwinata <99080684+FeaRisk@users.noreply.github.com>
2022-09-09 14:46:59 +0700

    fixed soal2

--- CH3-Kris/soal2.js
+++ CH3-Kris/soal2.js
@@ -7,7 +7,7 @@ class Animal {
         this.status = status
     }
     introduce() {
-        return `Hello, my name is ${this.name} and I am ${this.age} years old. I have ${this.legs}, I am a ${this.species}, and i ${this.status}`
+        return `Hello, my name is ${this.name} and I am ${this.age} years old. I am a ${this.species}, I have ${this.legs} legs, and i can ${this.status}`
     }
 }
 
@@ -21,3 +21,42 @@ class Shark extends Animal{
         return this.introduce()
     }
 }
+
+class Cat extends Animal{
+    constructor(name, age, status){
+        super(name, age, status)
+        this.legs = 4
+        this.species = "Cat"
+    }
+    show(){
+        return this.introduce()
+    }
+}
+
+class Dog extends Animal{
+    constructor(name, age, status, master){
+        super(name, age, status)
+        this.legs = 4
+        this.master = master
+        this.species = "Dog"
+
+    }
+    greetMaster(){
+        return `Hail my master ${this.master}. `
+    }
+    show(){
+        return this.greetMaster() + this.introduce()
+    }
+}
+
+let myShark = new Shark("Sharky", 21, "Swim")
+let myCat = new Cat("Fabregas", 5, "Scratch")
+let myDog = new Dog("Joergen", 4, "Bite", "Regis")
+
+console.log(myShark)
+console.log(myCat)
+console.log(myDog)
+
+console.log(myShark.show())
+console.log(myCat.show())
+console.log(myDog.show())
