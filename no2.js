42f098e50b7d6d6c2cf41518abf6b95f0f753bd8
mohfakhri <m.fakhri2604@gmial.com>
2022-09-09 09:31:55 +0700

    coba no 2

---
+++ no2.js
@@ -0,0 +1,40 @@
+class Animal {
+    constructor(name, age, legs, species, status) {
+        this.name = name;
+        this.age = age;
+        this.legs = legs;
+        this.species = species;
+        this.status = status;
+    }
+
+    introduce() {
+        return `Hello my name is ${this.name} and I am ${this.age} years old.`
+    }
+}
+
+class Shark extends Animal {
+    constructor(name, age, status) {
+        super(name, age, 0, 'shark', status);
+    }
+}
+
+class Cat extends Animal {
+    constructor(name, age, status) {
+        super(name, age, 4, 'cat', status);
+    }
+}
+
+class Dog extends Animal {
+    constructor(name, age, status, master) {
+        super(name, age, 4, 'dog', status);
+        this.master = master;
+    }
+
+    greetMaster() {
+        return `Hello ${this.master}`
+    }
+}
+
+let dog1 = new Dog("Bobby", 2, "Healthy", "sally");
+console.log(dog1.introduce())
+console.log(dog1.greetMaster())
