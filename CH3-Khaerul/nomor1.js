ebe7cd68cc7eb6fb3af57ad997b3b10ca51bfb2c
KHAERUL ANAM <96396662+kaanam@users.noreply.github.com>
2022-09-12 22:49:46 +0700

    add file

---
+++ CH3-Khaerul/nomor1.js
@@ -0,0 +1,18 @@
+function Ship(draft, crew) {
+  this.draft = draft;
+  this.crew = crew;
+}
+
+//class ship
+Ship.prototype.isWorthit = function () {
+  worth = this.draft + (this.crew + 1, 5);
+  if (worth > 20) {
+    return "Banyak nih muatan kapalnya";
+  } else {
+    return "Lebih banyak muatan orangnya..";
+  }
+};
+
+var emptyShip = new Ship(21, 10);
+
+console.log(emptyShip.isWorthit());
