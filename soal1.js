c17d5929871bd8ed4727fbe381d44f374eea678f
Kris Adiwinata <99080684+FeaRisk@users.noreply.github.com>
2022-09-15 23:11:32 +0700

    Fixed Chapter 3

---
+++ soal1.js
@@ -0,0 +1,11 @@
+// Soal No 1
+var emptyShip = new Ship (20, 10);
+
+function Ship (draft, crew){
+    this.draft = draft;
+    this.crew = crew;
+
+    draft = crew * 1.5 + draft;
+    
+    draft > 20 ? console.log (`Banyak nih muatan kapalnya (Muatan = ${draft})`) : console.log (`Lebih banyak muatan orangnya (Muatan = ${draft})`);
+}
